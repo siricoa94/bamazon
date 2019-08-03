b5c23f711851fdd6d21aa00da5ee973e297dbf1a
Andrew <siricoa94@github.com>
2019-08-02 22:24:32 -0400

    updated bamazonCustomer.js to display table above prompt in terminal. Also got code to pull from table

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,49 +1,74 @@
 var fs = require("fs");
 var mysql = require("mysql");
-var inquirer = require("inquirer")
+var inquirer = require("inquirer");
 
 var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "Adriana98!",
-    database: "bamazon"
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "Adriana98!",
+  database: "bamazon"
 });
-connection.connect(function(err) {
-    if (err) throw err;
-    console.log("connected as id " + connection.threadId);
-    printID();
-    // printItem();
-    connection.end();
+connection.connect(function (err) {
+  if (err) throw err;
+  console.log("connected as id " + connection.threadId);
+  // displayData();
+  printID();
+  // printItem();
+  // getItemInfo();
+
 });
-
-
-
-
-
-
-
-
-
-
-
 
 function printID() {
-connection.query("SELECT * FROM products", function(err, res) {
+  connection.query("SELECT id,product,department,stock,price FROM products", function (err, res) {
     if (err) throw err;
     console.log()
-    console.log(res);
+    console.table(res);
     console.log()
-});
-}
-// function printItem(item) {
-//     connection.query("SELECT * FROM products where product='" + item + "'", function(err, res) {
-//     if (err) throw err;
-//     console.log()
-//     console.log(res);
-//     console.log()
-    
-//     });
-// }
-
+    getItemInfo();
+  });
   
+}
+
+function getItemInfo() {
+  inquirer
+    .prompt([{
+        type: "input",
+        name: "item",
+        message: "Please Enter a Valid item ID: "
+      },
+      {
+        type: "input",
+        name: "units",
+        message: "How many units of this item would you like?"
+      }
+    ])
+    .then(function (response) {
+
+      // postInsert(response.item, response.units);
+      connection.query('select * from products where id=?', [response.item], function (err, data) {
+
+        if (response.units < data[0].stock) {
+          console.log('Your order is placed ...');
+          console.log(`Your total purchase price ${response.units * data[0].price}`)
+          let newQty = data[0].stock - response.units;
+          connection.query("UPDATE products set stock= ? where id=?", [newQty, response.item], function (err, res) {
+            if (err) throw err;
+            console.log('Table updated ');
+            // console.log();
+            connection.end();
+          });
+        } else {
+          console.log('Out of stock...');
+        }
+
+      });
+    });
+
+
+};
+
+// function postInsert(item, units) {
+
+
+// };
